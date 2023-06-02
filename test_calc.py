def83daf463f59a85a0f6cbcd03e003d6cc70f3b
VladislavVolkovS <105912739+VladislavVolkovS@users.noreply.github.com>
2023-06-02 08:14:42 +0300

    tests

---
+++ test_calc.py
@@ -0,0 +1,17 @@
+from main import *
+
+
+def test_add():
+    assert summ(2, 3) == 5
+
+
+def test_mul():
+    assert multyply(2, 3) == 6
+
+
+def test_div():
+    assert summ(6, 3) == 2
+
+
+def test_pow():
+    assert summ(2, 3) == 8
